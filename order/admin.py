23376c01f161c7c964d4f85f2125241d682fbc06
Kenneth Githambo <gdidder10@gmail.com>
2020-05-11 11:43:46 +0300

    empty commit

--- order/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from order.models import*
-
-# Register your models here.
-admin.site.register(Order)
-admin.site.register(Supplier)
-
-
--- user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
-
-# Create your views here.
-class RegisterView(CreateView):
-	form_class=UserCreationForm
-	template_name='register.html'
-	success_url='user:login'
